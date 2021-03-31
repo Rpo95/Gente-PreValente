9f485d86253250cea994a903372a13612d416fe9
Mateo Restrepo <67771954+Rpo95@users.noreply.github.com>
2021-03-31 11:23:29 -0500

    tercera entrega de PrevalentWare

--- components/input/input.js
+++ components/input/input.js
@@ -3,7 +3,7 @@ import styles from '../input/input.module.css'
 const Input = (props) => {
     return (
         <div className={`${styles.group}`}>      
-            <input className={`${styles.input}`}type="text" value={props.value}/>
+            <input className={`${styles.input}`} type="text" value={props.value}/>
             <span className={`${styles.highlight}`}></span>
             <span className={`${styles.bar}`}></span>
             <label className={`${styles.label}`}>{props.titleInput}</label>
